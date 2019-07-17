f8a497cdc8cd2935307b4eba4c5cae41f3dd0c36
satyamsingh1601 <satyamsingh19981601@gmail.com>
2019-07-17 09:45:13 +0530

    Solved : stacks -> Generate All Paranthesis

---
+++ stacks-queues/generate_all_paranthesis.py
@@ -0,0 +1,27 @@
+'''
+Generate al Paranthesis 
+Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+The brackets must close in the correct order, "()" and "()[]{}" are all valid but "(]" and "([)]" are not.
+
+Return 0 / 1 ( 0 for false, 1 for true ) for this problem
+
+'''
+
+class Solution:
+    def isValid(self, A):
+        stack = []
+        brackets = {']':'[','}':'{',')':'('}
+        for i in A:
+            if(i in brackets.values()):
+                stack.append(i)
+            elif i in brackets.keys() and len(stack)>0 and  brackets[i] == stack[-1]:
+                stack.pop()
+            else:
+                return 0
+        if(len(stack)>0):
+            return 0
+        return 1
+print(Solution().isValid('{]'))
+                
+                
